a18c3e54450cc856c17eed97d368e422acd6c5cf
Valery Kazachenko <Kazachenko.Valery.V@gmail.com>
2024-06-26 17:52:03 +1000

    Add project files.

---
+++ Bits.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace Develope_2_HomeWork
+{
+    internal class Bits
+    {
+        public long Number { get; private set; }
+        public byte Size { get; private set; }
+        public Bits(byte number)
+        {
+            Number = number;
+            Size = 7;
+        }
+        public Bits(short number)
+        {
+            Number = number;
+            Size = 15;
+        }
+        public Bits(int number)
+        {
+            Number = number;
+            Size = 31;
+        }
+        public Bits(long number)
+        {
+            Number = number;
+            Size = 63;
+        }
+        public bool GetBitByIndex(int index)
+        {
+            if (index < 0 || index > Size) throw new ArgumentOutOfRangeException("index is out of range");
+            return (Number & ((long)1 << index)) != 0;
+        }
+        public void SetBitByIndex(int index, bool value)
+        {
+            if (index < 0 || index > Size) throw new ArgumentOutOfRangeException("index is out of range");
+            long mask = ((long)1 << index);
+
+            if (value)
+                Number |= mask;          // Установка бита
+            else
+                Number &= (long)~mask;   // Сброс бита
+        }
+
+        // Индексатор для удобства вызова
+        public bool this[int index]
+        {
+            get => GetBitByIndex(index);
+            set => SetBitByIndex(index, value);
+        }
+
+        public override string ToString()
+        {
+            return $"{Number} -> {Convert.ToString(Number, 2).PadLeft(Size + 1, '0')}";
+        }
+
+        // Приведение типов --------------------------------------------------------
+        // Неявное приведение Bits -> byte
+        public static implicit operator byte(Bits b) => (byte)CheckRange(b.Number, byte.MinValue, byte.MaxValue);
+        // Явное приведение byte -> Bits
+        public static explicit operator Bits(byte b) => new Bits(b);
+
+        // Неявное приведение Bits -> short
+        public static implicit operator short(Bits b) => (short)CheckRange(b.Number, short.MinValue, short.MaxValue);
+        // Явное приведение short -> Bits
+        public static explicit operator Bits(short s) => new Bits(s);
+
+        // Неявное приведение Bits -> int
+        public static implicit operator int(Bits b) => (int)CheckRange(b.Number, int.MinValue, int.MaxValue);
+        // Явное приведение int -> Bits
+        public static explicit operator Bits(int i) => new Bits(i);
+
+        // Неявное приведение Bits -> long
+        public static implicit operator long(Bits l) => l.Number;
+        // Явное приведение long -> Bits
+        public static explicit operator Bits(long l) => new Bits(l);
+
+        private static long CheckRange(long value, long min, long max)
+        {
+            if ((value < min) || (value > max))
+                throw new InvalidCastException($"Value {value} is out of range for target type.");
+            return value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// Домашнее задание. Семинар 2. Интерфейсы и обобщения.
+// Реализуйте операторы явного и неявного приведения из
+// long, int, byte в Bits
+namespace Develope_2_HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работа через индексатор. Размер long");
+            Console.WriteLine("Запись 1 с последующим стиранием в каждый разряд:");
+
+            Bits b1 = new Bits((byte)0);
+            int size = sizeof(byte) * 8;
+            int j = 0;
+            Console.WriteLine($"Разряд {j} -> {(b1[j] ? 1 : 0)} => {b1.ToString()}");
+            for (int i = 1; i < size; i++)
+            {
+                b1[i - 1] = true;
+                Console.WriteLine($"Разряд {i} -> {(b1[i - 1] ? 1 : 0)} => {b1.ToString()}");
+                b1[i - 1] = false;
+            }
+            b1[size - 1] = true;
+            Console.WriteLine($"Разряд {j} -> {(b1[j] ? 1 : 0)} => {b1.ToString()}");
+            Console.WriteLine("---");
+
+            Bits l1 = new Bits((long)0);
+            size = sizeof(long) * 8;
+            j = 0;
+            Console.WriteLine($"Разряд {j} -> {(l1[j] ? 1 : 0)} => {l1.ToString()}");
+            for (int i = 1; i < size; i++)
+            {
+                l1[i - 1] = true;
+                Console.WriteLine($"Разряд {i} -> {(l1[i - 1] ? 1 : 0)} => {l1.ToString()}");
+                l1[i - 1] = false;
+            }
+            l1[size - 1] = true;
+            Console.WriteLine($"Разряд {j} -> {(l1[j] ? 1 : 0)} => {l1.ToString()}");
+
+            // Приведение типов
+            Console.WriteLine("\nПриведение типов");
+            long l2 = new Bits((long)1000); // Неявное
+            var l3 = (Bits)l2;              // Явное
+            Console.WriteLine($"Bits  -> long  : l2 = {l2}");
+            Console.WriteLine($"long  -> Bits  : l3 = {l3.ToString()}");
+
+            int i2 = new Bits((int)1000); // Неявное
+            var i3 = (Bits)i2;              // Явное
+            Console.WriteLine($"Bits  -> int   : i2 = {i2}");
+            Console.WriteLine($"int   -> Bits  : i3 = {i3.ToString()}");
+
+            short s2 = new Bits((short)1000); // Неявное
+            var s3 = (Bits)s2;              // Явное
+            Console.WriteLine($"Bits  -> short : s2 = {s2}");
+            Console.WriteLine($"short -> Bits  : s3 = {s3.ToString()}");
+
+            byte b2 = new Bits((byte)100); // Неявное
+            var b3 = (Bits)b2;              // Явное
+            Console.WriteLine($"Bits  -> byte  : b2 = {b2}");
+            Console.WriteLine($"byte  -> Bits  : b3 = {b3.ToString()}");
+        }
+    }
+}
